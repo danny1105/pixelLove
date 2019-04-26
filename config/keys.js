d6231ceef3e2a7229d3f44372f8229aaf627e242
danny1105 <ashishtiwari11@outlook.com>
2019-04-26 16:31:02 +0530

    Updated config file

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://test:test123@ds147746.mlab.com:47746/pair_pro" ,
+    mongoURI: "mongodb://test:test1234@ds137255.mlab.com:37255/mern-l1" ,
     secretOrKey: "secret"
 };
